0d75facae94af2ee3c728bb42953e96b572946d9
Y-G-Shin <yahya.goeksu@outlook.com>
2025-08-27 21:07:50 +0200

    NAVBAR + Angular issues

--- CCS/src/app/app.routes.ts
+++ CCS/src/app/app.routes.ts
@@ -1,3 +1,18 @@
-import { Routes } from '@angular/router';
+//import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { ShopComponent } from './pages/shop/shop';
+import { SoftwareComponent } from './pages/software/software';
+import { HireComponent } from './pages/hire/hire';
+
+export const routes: Routes = [
+    { path: '', component: ShopComponent},
+    { path: 'software', component: SoftwareComponent},
+    { path: 'hireMe', component: HireComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})export class AppRoutingModule { }
